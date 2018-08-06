c96cd3db4a38081793185286d9d105e74a6c62df
Doug <doug@localhost.localdomain>
2018-08-06 15:19:36 -0400

    changes

--- bliss-plugins/pug.js
+++ bliss-plugins/pug.js
@@ -1,17 +1,15 @@
-var pug = null
-
 class PugPlugin {
 	constructor() {
 		this.name = "Pug"
-		this.modules = ["pug"]
+		this.type = this.PREPROCESSOR
 		this.extensions = [".pug"]
 		this.output_extension = ".html"
 		this.might_have_frontmatter = true
-		pug = this.userRequire('pug')
+		this.requireWhenNeeded('pug')
 	}
 	render(text) {
-		if (pug) {
-			return pug.render(text)
+		if (this.modules['pug']) {
+			return this.modules['pug'].render(text)
 		} else {
 			this.fatal_error("Error. pug is null")
 		}
--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const glob = require("glob")
 const yaml = require('js-yaml')
 const fm   = require("./modules/frontmatter.js")
 
+const PREPROCESSOR_TYPE = 0
+const COMPILER_TYPE = 1
+const PRESENTATION_TYPE = 2
+
+var plugins = []   // Array of plugin classes
+// Map of plugins mapped to their desired extensions
+var pluginMap = [ {}, {}, {} ] // three objects are for TYPES defined above
+
 var siteOptions = {
 	"source": null,
 	"destination": "_site",
@@ -17,17 +25,18 @@ var siteOptions = {
 		"watch": false
 	}
 }
-var plugins = []   // Array of plugin classes
-var pluginMap = {} // Map of plugins mapped to their desired extensions
 
-try {
-	var loaded_config = yaml.safeLoad(fs.readFileSync('_config.yml', 'utf8'));
-	var jekyll_bliss_bkup = siteOptions['jekyll-bliss']
-	siteOptions = Object.assign({}, siteOptions, loaded_config)
-	siteOptions['jekyll-bliss'] =
-		Object.assign({}, siteOptions['jekyll-bliss'], jekyll_bliss_bkup)
-} catch (e) {
-	console.log("Not using a _config.yml - It doesn't exist");
+
+if (fs.existsSync("_config.yml")) {
+	try {
+		var loaded_config = yaml.safeLoad(fs.readFileSync('_config.yml', 'utf8'));
+		var jekyll_bliss_bkup = siteOptions['jekyll-bliss']
+		siteOptions = Object.assign({}, siteOptions, loaded_config)
+		siteOptions['jekyll-bliss'] =
+			Object.assign({}, siteOptions['jekyll-bliss'], jekyll_bliss_bkup)
+	} catch (e) {
+		fatal_error(e)
+	}
 }
 
 function fatal_error(...args) {
@@ -53,10 +62,13 @@ function ensureDirectoryExistence(filePath) {
 }
 
 const PluginPrototype = {
-	constructor() {
-		this.name = "Untitled Plugin"
-		this.modules = []
-	},
+	name: "Untitled Plugin",
+	type: null,
+	modules: {},
+	// "Constants"
+	PREPROCESSOR: PREPROCESSOR_TYPE,
+	COMPILER:     COMPILER_TYPE,
+	PRESENTATION: PRESENTATION_TYPE,
 	render(text) {
 		console.error("This plugin has not been implemented yet.")
 		return null
@@ -64,17 +76,23 @@ const PluginPrototype = {
 	fatal_error(...args) {
 		fatal_error.apply(null, args)
 	},
-	userRequire(module) {
-		import_path = path.join(process.cwd(), "node_modules", module)
-		try {
-			return require(import_path)
-		} catch(e) {
-			console.error("Failed to import module", module+".")
-			console.error("Please try 'npm install --save", module+"'")
-			console.error("Import Path:", import_path)
-			fatal_error()
+	requireWhenNeeded(module) {
+		this.modules[module] = null
+	},
+	importRequires(module) {
+		for (key in this.modules) {
+			if (!this.modules[key]) {
+				import_path = path.join(process.cwd(), "node_modules", key)
+				try {
+					this.modules[key] = require(import_path)
+				} catch(e) {
+					console.error("Failed to import module", key+".")
+					console.error("Please try 'npm install --save", key+"'")
+					console.error("Import Path:", import_path)
+					fatal_error()
+				}
+			}
 		}
-		return null
 	},
 }
 
@@ -87,12 +105,18 @@ glob(path.join(__dirname, "bliss-plugins/*.js"), function (er, files) {
 			Object.setPrototypeOf(Plugin.prototype, PluginPrototype);
 			var PluginObject = new Plugin()
 			var pluginlist_index = plugins.push(PluginObject) - 1
-			for (ext of PluginObject.extensions) {
-				if (pluginMap[ext]) {
-					pluginMap[ext].append(pluginlist_index)
-				} else {
-					pluginMap[ext] = [pluginlist_index]
+			var pluginType = PluginObject.type
+
+			if (pluginType == PREPROCESSOR_TYPE) {
+				for (ext of PluginObject.extensions) {
+					if (pluginMap[pluginType][ext]) {
+						pluginMap[pluginType][ext].append(pluginlist_index)
+					} else {
+						pluginMap[pluginType][ext] = [pluginlist_index]
+					}
 				}
+			} else {
+				pluginMap[pluginType][PluginObject.name] = pluginlist_index
 			}
 		} catch (e) {
 			console.error(e)
@@ -103,6 +127,8 @@ glob(path.join(__dirname, "bliss-plugins/*.js"), function (er, files) {
 
 var options = {
 	ignore: [
+		"package.json",
+		"package-lock.json",
 		idir('node_modules/'),
 		idir('.git/')
 	],
@@ -112,26 +138,35 @@ var options = {
 
 // Ignore the build folder
 var buildfolder = siteOptions['jekyll-bliss']['build-folder']
+var destination = siteOptions['destination']
+
 var searchPattern = "**/*"
+
 if (siteOptions['source']) {
 	searchPattern = path.join(
 		siteOptions['source'],
 		searchPattern
 	)
 }
-options['ignore'].push(idir(buildfolder))
 
-glob(searchPattern, options, function (er, files) {
+options['ignore'].push(idir(buildfolder))
+options['ignore'].push(idir(destination))
+
+function preprocessSite(files, callback) {
+	var misc_file_count = 0
+	var special_file_count = 0
+
 	// Make build folder if not exist
 	if (!fs.existsSync(buildfolder)){
 		fs.mkdirSync(buildfolder);
 	}
 
-	// The magic loop
+	// Loop over files and preprocess what is needed
 	for (file of files) {
 		var extension = path.extname(file)
-		var pluginMapValue = pluginMap[extension]
 		var outputFile = path.join(buildfolder, file)
+
+		// Tidy the outputFile
 		if (siteOptions['source']) {
 			var source = siteOptions['source']
 			var lastChar = source.substr(-1)
@@ -144,12 +179,13 @@ glob(searchPattern, options, function (er, files) {
 			)
 		}
 
-		console.log("Processing File", file)
-
-		if (pluginMapValue){
+		// **PREPROCCESOR CODE**
+		var pluginMapValue = pluginMap[PREPROCESSOR_TYPE][extension]
+		if (pluginMapValue){ // There exists a plugin for this filetype
 			var contents = fs.readFileSync(file, 'utf8') || ''
 			for (pluginIndex of pluginMapValue) {
 				var pluginObj = plugins[pluginIndex]
+				pluginObj.importRequires() // Import the required node modules
 
 				// Compiling the file.
 				// If it has front-matter, we process it separately
@@ -173,11 +209,22 @@ glob(searchPattern, options, function (er, files) {
 			// TODO Remove the src/ folder from pathname
 			ensureDirectoryExistence(outputFile)
 			fs.writeFileSync(outputFile, contents)
-		} else {
-			// Plugin does not exist to process file
+			console.log("[Preprocessor] Compiled", file)
+			special_file_count++
+		} else { // Plugin does not exist to process filetype
 			// Copy it to build folder instead
 			ensureDirectoryExistence(outputFile)
 			fs.createReadStream(file).pipe(fs.createWriteStream(outputFile))
+			misc_file_count++ // Increment the count of misc files processed
 		}
 	}
+	console.log("[Preprocessor] Finished.")
+	console.log("               Copied", misc_file_count, "misc file/s and compiled", special_file_count, "special file/s.")
+	if (callback) callback()
+}
+
+// Code
+glob(searchPattern, options, function (er, files) {
+	preprocessSite(files)
+	//compileSite()
 })
